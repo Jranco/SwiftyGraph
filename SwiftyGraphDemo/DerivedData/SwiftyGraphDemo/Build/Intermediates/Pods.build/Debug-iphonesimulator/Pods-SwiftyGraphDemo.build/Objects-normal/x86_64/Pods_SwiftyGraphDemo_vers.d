8898b38a57e72c908c6897833464d03298b64da0
Thomas Segkoulis <segkoulis@gmail.com>
2017-01-04 14:56:19 +0100

    removed derived data

--- SwiftyGraphDemo/DerivedData/SwiftyGraphDemo/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-SwiftyGraphDemo.build/Objects-normal/x86_64/Pods_SwiftyGraphDemo_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/thomas/Documents/Development/personal_projects/SwiftyGraph/SwiftyGraphDemo/DerivedData/SwiftyGraphDemo/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-SwiftyGraphDemo.build/DerivedSources/Pods_SwiftyGraphDemo_vers.c
--- SwiftyGraphDemo/DerivedData/SwiftyGraphDemo/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-SwiftyGraphDemoTests.build/DerivedSources/Pods_SwiftyGraphDemoTests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_SwiftyGraphDemoTestsVersionString[];
- extern const double Pods_SwiftyGraphDemoTestsVersionNumber;
-
- const unsigned char Pods_SwiftyGraphDemoTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_SwiftyGraphDemoTests  PROJECT:Pods-1" "\n";
- const double Pods_SwiftyGraphDemoTestsVersionNumber __attribute__ ((used)) = (double)1.;
